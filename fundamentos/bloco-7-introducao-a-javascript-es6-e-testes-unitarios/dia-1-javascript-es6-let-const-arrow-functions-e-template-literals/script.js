dfb6162754c96f7b732e4abdae14f9751d161060
Daniel Ramos Regis <danielrregis@gmail.com>
2022-07-29 13:12:52 -0300

    Questão 1 finalizada Parte 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,3 +1,5 @@
+//PARTE I
+
 // 🚀 Altere as variáveis para respeitarem o escopo em que foram declaradas.
 // Modifique a estrutura da função para que ela seja uma arrow function;
 // Modifique as variáveis para que respeitem o escopo onde estão declaradas;
@@ -22,6 +24,34 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 // Seu código aqui.
-const rearenge = (array) => array.sort((a, b) => a - b)
+const rearenge = (array) => {
+  array.sort((a, b) => a - b)
+  return `Os números ${array} se encontram ordenados de forma crescente!`
+}
 
 console.log(rearenge(oddsAndEvens)); // será necessário alterar essa linha 😉
+
+// Crie uma função que faça com que o array oddsAndEvens fique na ordem crescente;
+// Utilize o console.log e template literals para retornar a frase: 'Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!'.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+//PARTE II
+
+
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número inteiro e positivo N, representado por N!, é o produto de todos os seus antecessores até o número um. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+
+const fatorial = (num) => {
+  let resultado = 1
+  if (num === 0) {
+    resultado = 1
+  }
+  for (let index = 1; index <= num; index += 1) {
+    resultado = resultado * index
+  }
+  return resultado
+}
+
+console.log(fatorial(0));
