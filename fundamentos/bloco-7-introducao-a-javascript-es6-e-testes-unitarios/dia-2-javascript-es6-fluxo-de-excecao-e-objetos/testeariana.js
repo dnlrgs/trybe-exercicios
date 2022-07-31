c94aaa7886a11761d9fdae870def45684488c1de
Daniel Ramos Regis <danielrregis@gmail.com>
2022-07-31 12:02:07 -0300

    parte-2-exercicio-1 iniciado

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/testeariana.js
@@ -0,0 +1,17 @@
+
+
+
+
+const gastosDoMes = [10, 30, 170, 97, 55] 
+
+
+
+
+
+for (let index = gastosDoMes.length - 1; index >= 0 ; index -= 1) {
+    console.log(gastosDoMes[index]);
+    
+}
+
+
+console.log(typeof gastosDoMes.length);
